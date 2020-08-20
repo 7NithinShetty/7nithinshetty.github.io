3dc623eb140a9bad037c00aaddeecc7e1f53c36f
NthnShtty <shettynithin007@gmail.com>
2020-08-20 22:26:08 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    //--------------------------------------------Nav Bar Starts--------------------------------------------
+    // const navslide = () => {
+    //     const hamburger = document.querySelector('.hamburger');
+    //     const nav = document.querySelector('.nav-links');
+    //     const navlinks = document.querySelectorAll('.nav-links li');
+
+    //     hamburger.addEventListener('click', () => {
+    //         //toggle nav
+    //         nav.classList.toggle('nav-active');
+
+    //         //Animate links
+    //         navlinks.forEach((link, index) => {
+    //             if (link.style.animation) {
+    //                 link.style.animation = ''
+    //             } else {
+    //                 link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+    //             }
+    //         });
+    //         //Burger Animation
+    //         hamburger.classList.toggle('cross-bar');
+    //     });
+    // }
+
+    // navslide();
+    //--------------------------------------------Nav Bar Ends-----------------------------------------------
+});
