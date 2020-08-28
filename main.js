2fc49e352e70557d6b5ee463e90962823fb0a1e4
NthnShtty <shettynithin007@gmail.com>
2020-08-29 00:27:39 +0530

    final touche1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+
     window.addEventListener('scroll', function(){
         var nav = document.querySelector('nav');
         nav.classList.toggle('stick', window.scrollY > 0);
@@ -11,29 +11,38 @@ $(document).ready(function() {
         hamburger.classList.toggle('active');
         navlinks.classList.toggle('active');
     });
-    //--------------------------------------------Nav Bar Starts--------------------------------------------
-    // const navslide = () => {
-    //     const hamburger = document.querySelector('.hamburger');
-    //     const nav = document.querySelector('.nav-links');
-    //     const navlinks = document.querySelectorAll('.nav-links li');
+    const hme = document.querySelector('.hme');
+    hme.addEventListener('click', function () {
+        hamburger.classList.toggle('active');
+        navlinks.classList.toggle('active');
+    });
+    const abt = document.querySelector('.abt');
+    abt.addEventListener('click', function () {
+        hamburger.classList.toggle('active');
+        navlinks.classList.toggle('active');
+    });
+    const edu = document.querySelector('.edu');
+    edu.addEventListener('click', function () {
+        hamburger.classList.toggle('active');
+        navlinks.classList.toggle('active');
+    });
+    const cont = document.querySelector('.cont');
+    cont.addEventListener('click', function () {
+        hamburger.classList.toggle('active');
+        navlinks.classList.toggle('active');
+    });
 
-    //     hamburger.addEventListener('click', () => {
-    //         //toggle nav
-    //         nav.classList.toggle('nav-active');
-
-    //         //Animate links
-    //         navlinks.forEach((link, index) => {
-    //             if (link.style.animation) {
-    //                 link.style.animation = ''
-    //             } else {
-    //                 link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-    //             }
-    //         });
-    //         //Burger Animation
-    //         hamburger.classList.toggle('cross-bar');
-    //     });
-    // }
-
-    // navslide();
-    //--------------------------------------------Nav Bar Ends-----------------------------------------------
-});
+    const navs = document.querySelectorAll('.nav-links li');
+    hamburger.addEventListener('click', () => {
+        navs.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7}s`;
+            }
+        });
+    });
+    const educolor = document.querySelector('.education');
+    educolor.addEventListener('click', function () {
+        document.getElementsByClassName('stick').setAttribute('background', 'green');
+    });
